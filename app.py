900b20c99d2cd9e2a97a6a964839897da3d1517d
athreyaupadhyaya18-sudo <athreyaupadhyaya18@gmail.com>
2025-11-05 06:25:07 +0530

    Updated for Render deployment

--- app.py
+++ app.py
@@ -12,4 +12,5 @@ def index():
     return render_template("index.html", translated_text=translated_text)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
+
