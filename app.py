c64c8ae6b30f7709e05352114b13de5bf3164d48
athreyaupadhyaya18-sudo <athreyaupadhyaya18@gmail.com>
2025-11-05 06:05:24 +0530

    Initial translator commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+from deep_translator import GoogleTranslator
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    translated_text = ""
+    if request.method == "POST":
+        text_to_translate = request.form["text"]
+        translated_text = GoogleTranslator(source='english', target='kannada').translate(text_to_translate)
+    return render_template("index.html", translated_text=translated_text)
+
+if __name__ == "__main__":
+    app.run(debug=True)
